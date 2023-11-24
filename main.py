90d1e7555ed166533b7b92bfdb58ba9a3e0fd50e
Martijn van Noord (mint) <martijn@vannoord.nu>
2023-11-24 20:22:26 +0100

    Initial commmit

---
+++ main.py
@@ -0,0 +1,22 @@
+'Easy interfacing with the moneybird'
+
+class MoneybirdClient():
+
+    def __init__(self, key):
+        self.key = key
+
+    def get_administrations(self) -> list[str]:
+        pass
+
+    def get_invoices(self):
+        pass
+
+    def get_customers(self):
+        pass
+
+    def get_timeentries(self):
+        pass
+
+
+
+
